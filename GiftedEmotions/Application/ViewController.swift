2f19e9260f29070a4bbb142b7d0fce0e03797a3d
resoul <resoul@bk.ru>
2026-02-09 00:10:28 +0200

    GF - setup LaunchScreen and base color

---
+++ GiftedEmotions/Application/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Gifted Emotions"
+        view.backgroundColor = UIColor(named: "AccentColor")
+    }
+}
